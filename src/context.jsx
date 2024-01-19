c7ea7e37498f23f2fbf4b3a1e1be767f6b360cf1
miawithcode <cuttontail@gmail.com>
2024-01-20 00:47:53 +0800

    feat: add children prop

--- src/context.jsx
+++ src/context.jsx
@@ -2,11 +2,13 @@ import { createContext, useState } from 'react';
 
 const GlobalContext = createContext();
 
-const AppContext = () => {
+const AppContext = ({ children }) => {
   const [name, setName] = useState('peter');
 
   return (
-    <GlobalContext.Provider value={{ name, setName }}></GlobalContext.Provider>
+    <GlobalContext.Provider value={{ name, setName }}>
+      {children}
+    </GlobalContext.Provider>
   );
 };
 
